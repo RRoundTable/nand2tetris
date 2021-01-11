bd720d90520fe66fa06b7e3e7152c43c3d6f8a85
RRoundTable <ryu071511@gmail.com>
2021-01-11 22:16:21 +0900

    add

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,55 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@i
+M = 0
+
+(CHECK)
+    @i
+    D = M
+    @RESET
+    D; JLT
+    @KBD
+    D = M
+    @LOOP2
+    D; JEQ
+
+    @LOOP1
+    D; JNE
+
+(LOOP1) 
+    @i
+    D = M
+
+    @SCREEN
+    A = A + D
+    M = -1
+    @i
+    M = M + 1
+
+
+    @CHECK
+    0; JMP
+
+    @KBD
+    M = 0
+
+(LOOP2)
+    @i
+    D = M
+    @SCREEN
+    A = A + D
+    M = 0
+    @i
+    M = M - 1
+    @CHECK
+    0; JMP
+    @KBD
+    M = 0
+
+(RESET)
+    @i
+    M = 0
+    @CHECK
+    0; JMP
